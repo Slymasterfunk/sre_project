1c0136f16896a0da2318a5ad9dbec834b85cbbc5
Sly <slymasterfunk@gmail.com>
2020-04-13 13:55:48 -0500

    Email issue

--- contacts/views.py
+++ contacts/views.py
@@ -12,7 +12,6 @@ def contact(request):
     phone = request.POST['phone']
     message = request.POST['message']
     user_id = request.POST['user_id']
-    realtor_email = request.POST['realtor_email']
 
     # Check if user has made inquiry already
     if request.user.is_authenticated:
@@ -25,15 +24,6 @@ def contact(request):
     contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
 
     contact.save()
-
-    # Send email
-    send_mail(
-      'Property Listing Inquiry',
-      'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
-      'slymasterfunk@gmail.com',
-      [realtor_email, 'slymasterfunk@gmail.com'],
-      fail_silently = False
-    )
 
     messages.success(request, 'Your request has been submitted, a realtor will get back to you soon.')
     return redirect('/listings/'+listing_id)
