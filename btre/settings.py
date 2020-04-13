87208e8df60625798ab4906bcbfd03e8ed1214b8
Sly <slymasterfunk@gmail.com>
2020-04-13 11:14:03 -0500

    Removed email

--- btre/settings.py
+++ btre/settings.py
@@ -141,13 +141,6 @@ MESSAGE_TAGS = {
     messages.ERROR:'danger'
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'slymasterfunk@gmail.com'
-EMAIL_HOST_PASSWORD = 'chimps244677'
-EMAIL_USE_TLS = True
-
 try:
     from .local_settings import *
 except ImportError:
